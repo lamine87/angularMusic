fd6110aaa406e25ae9b4923cbd2b43c6c2b3aaa6
lamine87 <laminenatidiarra@yahoo.fr>
2022-05-17 12:38:37 +0200

    M23Front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: "", redirectTo: "/home", pathMatch:"full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
